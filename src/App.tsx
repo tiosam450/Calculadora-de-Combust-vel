a70f974e91de0e51d23dd48fd8d4e8c1d55bd7b7
Samuel <tiosam450@hotmail.com>
2024-07-18 13:30:27 -0400

    Desk MV

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import TesteProps from "./TesteProps";
+import { useState } from 'react'
+import icone from './assets/img/icone_combustivel.png'
 
 export default function App() {
+  const [alcool, setAlcool] = useState('');
+  const [gasolina, setGasolina] = useState('')
+  
+  function calcular(){
+
+  }
+  
   return (
     <>
-      <h1>React + Typescript</h1>
-      <Nome idade={30}/>
-      <TesteProps teste='Oi testando som' volume={12}/>
+      <div className='containerApp'>
+        <div className="container">
+          <img className='icone' src={icone} alt="imagem" />
+          <h2>Álcool ou Gasolina?</h2>
+
+          <form onSubmit={calcular}>
+            <label htmlFor="">Preço do álcool</label>
+            <input type="text" value={alcool} placeholder='Digite o preço do álcool' onChange={e=>setAlcool(e.target.value)}/>
+            <label htmlFor="">Preço da gasolina</label>
+            <input type="text" value={gasolina} placeholder='Digite o preço da gasolina' onChange={e=>setGasolina(e.target.value)}/>
+            <button>Calcular</button>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
-
-interface NomeProps{
-  nome?: string,
-  idade?: number,
-}
-
-function Nome ({nome="Default", idade}:NomeProps){
-  return(
-    <div>
-      <h1>Nome: {nome}</h1>
-      <h3>idade: {idade}</h3>
-    </div>
   )
 }
